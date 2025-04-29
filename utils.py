ec8b3f91c1631c88cee489141685326ca8a2b452
serpa <122547358+onioktairawan@users.noreply.github.com>
2025-04-29 15:18:29 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,19 +1,17 @@
-import time
-import psutil
 from datetime import datetime, timezone
+import psutil, platform
 
-# Catat waktu mulai bot
-BOT_START_TIME = time.time()
+start_time = datetime.now()
 
-def get_uptime() -> str:
-    uptime_seconds = time.time() - BOT_START_TIME
-    return time.strftime("%H:%M:%S", time.gmtime(uptime_seconds))
+def get_uptime():
+    delta = datetime.now() - start_time
+    return str(delta).split('.')[0]
 
-def get_system_stats() -> dict:
-    return {
-        "cpu": psutil.cpu_percent(),
-        "ram": psutil.virtual_memory().percent
-    }
+def get_latency(message_date):
+    message_date = message_date.astimezone(timezone.utc)
+    now = datetime.now(timezone.utc)
+    return round((now - message_date).total_seconds() * 1000, 2)
 
-def get_ping(message_date: datetime) -> float:
-    return round((datetime.now(timezone.utc) - message_date).total_seconds() * 1000, 2)
+def get_cpu_info():
+    cpu_percent = psutil.cpu_percent(interval=0.5)
+    return f"{cpu_percent}% (Platform: {platform.system()})"
