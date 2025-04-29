f9bf4a2c8f1d531449629bd49bdb6f115a33168a
serpa <122547358+onioktairawan@users.noreply.github.com>
2025-04-29 15:32:21 +0700

    Update getstring.py

--- getstring.py
+++ getstring.py
@@ -1,17 +1,24 @@
 from pyrogram import Client
+from pyrogram.errors import SessionPasswordNeeded
 import asyncio
-import io
+import os
 
-async def create_string_session(api_id, api_hash):
-    output = io.StringIO()
+async def create_user_string(api_id, api_hash, phone_number, send_code_func, ask_password_func):
+    session_name = f"sessions/{phone_number}"
+    app = Client(session_name, api_id=api_id, api_hash=api_hash)
 
-    async with Client(
-        name="session",
-        api_id=api_id,
-        api_hash=api_hash,
-        in_memory=True
-    ) as app:
-        string = await app.export_session_string()
-        output.write(string)
+    await app.connect()
+    if not await app.check_authorization():
+        code = await send_code_func(phone_number, app)
+        try:
+            await app.sign_in(phone_number, code)
+        except SessionPasswordNeeded:
+            password = await ask_password_func()
+            await app.check_password(password)
 
-    return output.getvalue()
+    string_session = await app.export_session_string()
+    me = await app.get_me()
+    await app.send_message("me", f"✅ Ini string session Anda:\n\n`{string_session}`\n\n🔐 Jangan bagikan ke siapa pun!")
+    await app.disconnect()
+
+    return string_session, me.first_name
